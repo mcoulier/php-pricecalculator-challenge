b64c9ae44a27c1cdf57b7367fd35822132b2e79b
glezzz <alexglezk@gmail.com>
2020-10-20 12:04:36 +0200

    created product class

--- Model/Product.php
+++ Model/Product.php
@@ -1,6 +1,46 @@
 <?php
+
 class Product
 {
+    private int $id;
+    private string $name;
+    private float $price;
 
+    /**
+     * Product constructor.
+     * @param int $id
+     * @param string $name
+     * @param float $price
+     */
+    public function __construct(int $id, string $name, float $price)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
 
 }
