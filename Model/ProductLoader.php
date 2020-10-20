51d638510ad3bef26d3335a42a5c9be8049722d2
Michiel Coulier <michielcoulier@gmail.com>
2020-10-20 14:23:55 +0200

    Getting product data in array

--- Model/ProductLoader.php
+++ Model/ProductLoader.php
@@ -1,14 +1,16 @@
 <?php
 
-require 'Model/Connection.php';
-
 class ProductLoader extends Connection
 {
+    private array $products = [];
+
     public function __construct()
     {
         $handle = $this->openConnection()->prepare('SELECT * FROM product');
         $handle->execute();
+        foreach ($handle->fetchAll() as $product){
+            $this->products[] = new Product($product['id'], $product['name'], $product['price']);
+        }
+    /*    $result = $handle->fetchAll();*/
     }
-
 }
-
