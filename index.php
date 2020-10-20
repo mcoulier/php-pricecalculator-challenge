131dad6e17fc1b8fabe473d493f9ea9ddb068093
Michiel Coulier <michielcoulier@gmail.com>
2020-10-20 16:43:41 +0200

    link to homepagecontroller

--- index.php
+++ index.php
@@ -6,15 +6,10 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-require_once 'Model/Connection.php';
-require_once 'Model/Product.php';
-require_once 'Model/ProductLoader.php';
+require 'Model/Connection.php';
+require 'Model/Product.php';
+require 'Model/ProductLoader.php';
+require 'Controller/HomepageController.php';
 
-$products = new ProductLoader();
-
-$productsArray = $products->getProducts();
-
-foreach ($productsArray as $product){
-    echo $product->getName();
-    echo $product->getPrice();
-}
+$controller = new HomepageController();
+$controller->render($_GET, $_POST);
