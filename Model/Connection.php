cc087f60d52ca88b651dc9bcffad432fa8b25377
Michiel Coulier <michielcoulier@gmail.com>
2020-10-20 09:30:33 +0200

    Model connection upload

---
+++ Model/Connection.php
@@ -0,0 +1,17 @@
+<?php
+function openConnection() : PDO {
+// Try to figure out what these should be for you
+require 'resources/config.php';
+
+$driverOptions = [
+PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+// Try to understand what happens here
+$pdo = new PDO('mysql:host='. $dbhost .';dbname='. $db, $dbuser, $dbpass, $driverOptions);
+
+// Why we do this here
+return $pdo;
+}
