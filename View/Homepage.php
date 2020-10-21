f245612a0bf7b73eb3a293a98a24f163cc845343
glezzz <alexglezk@gmail.com>
2020-10-21 14:20:34 +0200

    wotking on homeÃpage

--- View/Homepage.php
+++ View/Homepage.php
@@ -11,11 +11,26 @@
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-<select name="product" id="product">
-    <?php foreach ($products->getProducts() AS $product){?>
-        <option value="<?php echo $product->getPrice() ?>"><?php echo $product->getName()?></option>
+<form method="post">
+<label for="customer">Customer</label>
+<select name="customer" id="customer">
+    <?php foreach ($customers AS $cust){?>
+        <option value="<?php echo $cust->getId() ?>"><?php echo $cust->getFirstname() . " " . $cust->getLastname()?></option>
     <?php }?>
 </select>
+
+<label for="product">Product</label>
+<select name="product" id="product">
+    <?php foreach ($products AS $product){?>
+        <option value="<?php echo $product->getId() ?>"><?php echo $product->getName()?></option>
+    <?php }?>
+</select>
+<button type="submit">Submit</button>
+</form>
+<hr/>
+<?php if ($customer){?>
+    <?php echo $customer->getFirstname() ?>
+<?php }?>
 
 
 
