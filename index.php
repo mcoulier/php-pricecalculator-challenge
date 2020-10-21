0b02c9fc0aa24f5981131a1b9b5bdda5f7ba0586
Michiel Coulier <michielcoulier@gmail.com>
2020-10-21 11:36:35 +0200

    adding required php files

--- index.php
+++ index.php
@@ -9,6 +9,10 @@ error_reporting(E_ALL);
 require 'Model/Connection.php';
 require 'Model/Product.php';
 require 'Model/ProductLoader.php';
+require 'Model/Customer.php';
+require 'Model/CustomerLoader.php';
+require 'Model/CustomerGroup.php';
+require 'Model/CustomergroupLoader.php';
 require 'Controller/HomepageController.php';
 
 $controller = new HomepageController();
