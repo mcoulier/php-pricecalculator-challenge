feff9b1b0ef15ea4c52baa6aabd90ce8bb15aedb
glezzz <alexglezk@gmail.com>
2020-10-22 15:59:45 +0200

    testing 2 decimals after comma

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -13,127 +13,117 @@ class HomepageController
         $customerGroups = new CustomerGroupLoader();
         $getCustomerGroups = $customerGroups->getCustomerGroups();
 
-        function loopArray($array, $value){
-            foreach ($array as $customerGroup){
-                if ($customerGroup->getId() == $value){
+        function loopArray($array, $value)
+        {
+            foreach ($array as $customerGroup) {
+                if ($customerGroup->getId() == $value) {
                     return $customerGroup;
                 }
             }
-
         }
 
-        //if this is form submit try to calc the final price
         $varDiscounts = [];
-        //fetch the selected customer from the db
-        if(isset($POST['customers']) && isset($POST['product'])){
+        if (isset($POST['customers']) && isset($POST['product'])) {
             $customerData = $POST['customers'];
             $productData = $POST['product'];
             $result = explode(",", $customerData);
             $groupId = $result[0];
-            var_dump($result);
+            //var_dump($result);
             $productArray = explode(",", $productData);
             $productBasePrice = $productArray[0];
-            /*                var_dump($object);*/
-
-            /*                $customer = $customers->getCustomerById($customerId);*/
+            //var_dump($object);
+            //$customer = $customers->getCustomerById($customerId);
 
             $object = loopArray($getCustomerGroups, $groupId);
-            if($object->getVariableDiscount() == null){
+            if ($object->getVariableDiscount() == null) {
                 array_push($varDiscounts, $object);
 
-                do{
+                do {
                     $object = loopArray($getCustomerGroups, $object->getParentId());
                     array_push($varDiscounts, $object);
 
-                } while($object->getParentId() !== null);
+                } while ($object->getParentId() !== null);
 
-            } elseif ($object->getVariableDiscount() !== null){
+            } elseif ($object->getVariableDiscount() !== null) {
                 array_push($varDiscounts, $object);
 
-                do{
+                do {
                     $object = loopArray($getCustomerGroups, $object->getParentId());
                     array_push($varDiscounts, $object);
 
-                }while($object->getParentId() !== null);
+                } while ($object->getParentId() !== null);
             }
 
             $variableDiscount = 0;
             $fixedDiscount = 0;
 
-            for ($i = 0; $i < count($varDiscounts); $i++){
+            for ($i = 0; $i < count($varDiscounts); $i++) {
 
-                if($variableDiscount < $varDiscounts[$i]->getVariableDiscount()){
+                if ($variableDiscount < $varDiscounts[$i]->getVariableDiscount()) {
                     $variableDiscount = $varDiscounts[$i]->getVariableDiscount();
                 }
-                if ($varDiscounts[$i]->getFixedDiscount() != null){
+                if ($varDiscounts[$i]->getFixedDiscount() != null) {
                     $fixedDiscount += $varDiscounts[$i]->getFixedDiscount();
                 }
             }
 
-            echo "this is variable ".$variableDiscount;
-            echo "this is fixed ".$fixedDiscount;
+            echo "this is variable " . $variableDiscount;
+            echo "this is fixed " . $fixedDiscount;
 
-            $variable = ($productBasePrice/100)*$variableDiscount;
-            $fixed = $fixedDiscount*100;
+            $variable = ($productBasePrice / 100) * $variableDiscount;
+            $fixed = $fixedDiscount * 100;
             $discount = 0;
             $varOrFixed = "";
 
-            if($variable > $fixed){
+            if ($variable > $fixed) {
                 $discount = $variableDiscount;
                 $varOrFixed = "variable";
+
             } else {
                 $discount = $fixed;
                 $varOrFixed = "fixed";
             }
 
-            if($varOrFixed == "variable" && $result[2] != ""){
-                if ($discount < $result[2]){
+            if ($varOrFixed == "variable" && $result[2] != "") {
+                if ($discount < $result[2]) {
                     $discount = $result[2];
                 }
             }
 
             $totalPrice = 0;
-
-            if($varOrFixed == "variable" && $result[2] != ""){
-                $percent = ($productBasePrice/100)*$discount;
+            if ($varOrFixed == "variable" && $result[2] != "") {
+                $percent = ($productBasePrice / 100) * $discount;
                 $totalPrice = $productBasePrice - $percent;
-            } elseif ($varOrFixed == "variable" && $result[2] == ""){
-                $totalPrice = $productBasePrice - ($result[1]*100);
-                $percent = ($totalPrice/100)*$discount;
+
+            } elseif ($varOrFixed == "variable" && $result[2] == "") {
+                $totalPrice = $productBasePrice - ($result[1] * 100);
+                $percent = ($totalPrice / 100) * $discount;
                 $totalPrice = $totalPrice - $percent;
 
-            } elseif ($varOrFixed == "fixed" && $result[2] != ""){
+            } elseif ($varOrFixed == "fixed" && $result[2] != "") {
                 $totalPrice = $productBasePrice - $discount;
-                $percent = ($totalPrice/100)*$result[2];
+                $percent = ($totalPrice / 100) * $result[2];
                 $totalPrice = $totalPrice - $percent;
+
             } else {
                 $totalPrice = $productBasePrice - $discount;
-                $totalPrice = $productBasePrice - ($result[1]*100);
+                $totalPrice = $productBasePrice - ($result[1] * 100);
+                $discountDecimals = number_format($discount, 2);
             }
 
-            if($totalPrice < 0){
+            //price can't be under 0
+            if ($totalPrice < 0) {
                 $totalPrice = 0;
             }
 
 
             var_dump($varDiscounts);
 
-/*            $varDiscount = $varDiscounts[0]->getVariableDiscount();
+            /*            $varDiscount = $varDiscounts[0]->getVariableDiscount();
 
-            var_dump($varDiscount);*/
+                        var_dump($varDiscount);*/
 
         }
-        /*if (isset($POST['product'])){
-            $productId = $POST['product'];
-            $productArray = explode(",", $productId);
-            var_dump($productArray);
-            $productBasePrice = $productArray[0];
-
-        }*/
-
-
-
-
 
         require 'View/Homepage.php';
     }
