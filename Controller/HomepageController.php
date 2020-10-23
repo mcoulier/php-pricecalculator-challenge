f4f1acd5e013f5319acc24a3ed66480c7503d254
glezzz <alexglezk@gmail.com>
2020-10-23 10:21:46 +0200

    addig some more comments

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -38,7 +38,7 @@ class HomepageController
             //var_dump($object);
             //$customer = $customers->getCustomerById($customerId);
 
-            //loop through all data to get groups and discounts
+            //loop through all data to get parent IDs
             $object = loopArray($getCustomerGroups, $groupId);
             if ($object->getVariableDiscount() == null) {
                 array_push($customerData, $object);
@@ -61,7 +61,7 @@ class HomepageController
 
             $variableDiscount = 0;
             $fixedDiscount = 0;
-            //
+            //get the correct variable discount & the fixed discount
             for ($i = 0; $i < count($customerData); $i++) {
 
                 if ($variableDiscount < $customerData[$i]->getVariableDiscount()) {
@@ -72,10 +72,9 @@ class HomepageController
                 }
             }
 
-            echo "this is variable " . $variableDiscount;
-            echo "this is fixed " . $fixedDiscount;
-
+            //convert product base price to float to be able to calc its variable discount (it being a percentage)
             $variable = ($productBasePrice / 100) * $variableDiscount;
+            // * 100 to be able to subtract correctly from DB price
             $fixed = $fixedDiscount * 100;
             $discount = 0;
             $varOrFixed = "";
@@ -95,6 +94,7 @@ class HomepageController
                 }
             }
 
+            //calculate total price
             $totalPrice = 0;
             if ($varOrFixed == "variable" && $result[2] != "") {
                 $percent = ($productBasePrice / 100) * $discount;
